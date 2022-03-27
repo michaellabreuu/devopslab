f8f378fa4309b46193d6791ac2c82299e23ce6bb
Michael Abreu <michaellabreuu@hotmail.com>
2022-03-27 17:03:08 -0300

    Test Fail Pipeline

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def pagina_inicial():
-    return "Hello World - Michael Abreu"
+    return "Hello World - Michael Abreu - FIAP"
 
 if __name__ == '__main__':
     app.run()
