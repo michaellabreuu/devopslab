f25fcaac933e3145ec88abfd2a8aefd4b1306afd
Michael Abreu <michaellabreuu@hotmail.com>
2022-03-30 13:53:57 -0300

    Update App

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ csrf = CSRFProtect(app)
 
 @app.route("/")
 def pagina_inicial():
-    return "Laboratório Pipeline DevOps"
+    return "Laboratório Pipeline DevOps - FIAP"
 
 if __name__ == '__main__':
     port = os.getenv('PORT')
